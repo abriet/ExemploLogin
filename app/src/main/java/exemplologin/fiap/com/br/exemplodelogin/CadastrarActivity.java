a8791ece05f7d08c0cd8e4c31ffefdbb8da64c75
Alexandre Briet <alexandrerusilas@gmail.com>
2017-02-16 11:56:05 -0200

    Iniciando o desafio do gerador de imagens

--- app/src/main/java/exemplologin/fiap/com/br/exemplodelogin/CadastrarActivity.java
+++ app/src/main/java/exemplologin/fiap/com/br/exemplodelogin/CadastrarActivity.java
@@ -2,12 +2,27 @@ package exemplologin.fiap.com.br.exemplodelogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CadastrarActivity extends AppCompatActivity {
+
+    EditText edtUser;
+    EditText edtPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar);
+
+        edtUser = (EditText) findViewById(R.id.edtNewUser);
+        edtPass = (EditText) findViewById(R.id.edtNewPass);
+    }
+
+    public void cadastrarUsuario(View v){
+        String user = edtUser.getText().toString();
+        String pass = edtPass.getText().toString();
+
+        
     }
 }
